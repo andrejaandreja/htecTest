67befe03b7a87c00dc2c0ab986891eea451b2727
Andreja <andreja.stanojkovic@gmail.com>
2017-11-30 23:15:32 +0100

    1.0

--- structure/controller.js
+++ structure/controller.js
@@ -15,6 +15,27 @@ class Dispatcher {
 class CarController {
     // deklarisanje potrebnih vrednosti i poziv funkcija za iscrtavanje kontrola
     constructor(mainDiv, jsonService) {
+        this.mainDiv = mainDiv;
+        jsonService.getData(this, "json/data.json", null, this.prepareCarDivs);
+    }
 
+    prepareCarDivs(caller, data){
+        caller.data = data;
+        // kreiranje divova
+        const containerDiv = document.createElement("div");
+        const rowDiv = document.createElement("div");
+        const carsSearchDiv = document.createElement("div");
+        const carsDiv = document.createElement("div");
+
+        // dodeljivanje atributa
+        containerDiv.className = "container";
+        rowDiv.className = "row";
+        carsSearchDiv.className = "search-control";
+
+        // povezivanje kontrola
+        caller.mainDiv.appendChild(containerDiv);
+        containerDiv.appendChild(rowDiv);
+        rowDiv.appendChild(carsSearchDiv);
+        rowDiv.appendChild(carsDiv);
     }
 }
